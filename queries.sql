66d32abbfe3dd65c416a2d372ffd10d94a283dba
Hunter Cash <hmclaug@gmail.com>
2019-04-09 23:13:17 -0500

    added more queries

--- queries.sql
+++ queries.sql
@@ -43,21 +43,39 @@ ALTER TABLE actor DROP COLUMN description;
 SELECT last_name, COUNT(*) FROM actor GROUP BY last_name;
 
 -- 4b. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
+SELECT last_name, COUNT(*) FROM actor GROUP BY last_name HAVING COUNT(*)>=2;
 
 -- 4c. The actor `HARPO WILLIAMS` was accidentally entered in the `actor` table as `GROUCHO WILLIAMS`. Write a query to fix the record.
+-- find groucho williams
+SELECT first_name, last_name, actor_id FROM actor WHERE first_name='GROUCHO' AND last_name='WILLIAMS'; 
+-- ID = 172
+-- Fix the record
+UPDATE actor SET first_name = 'HARPO' WHERE (first_name='GROUCHO' AND last_name='WILLIAMS');
+-- See if the record is fixed
+SELECT first_name, last_name, actor_id FROM actor WHERE first_name='HARPO';
+
 
 -- 4d. Perhaps we were too hasty in changing `GROUCHO` to `HARPO`. It turns out that `GROUCHO` was the correct name after all! In a single query, if the first name of the actor is currently `HARPO`, change it to `GROUCHO`.
-
+UPDATE actor SET first_name = "GROUCHO" WHERE first_name = "HARPO";
+SELECT actor_id, first_name, last_name FROM actor WHERE actor_id=172;
 /* --------------------------------------- */ 
 
 -- 5a. You cannot locate the schema of the `address` table. Which query would you use to re-create it?
-
+SHOW CREATE TABLE address;
 /* --------------------------------------- */ 
 
 -- 6a. Use `JOIN` to display the first and last names, as well as the address, of each staff member. Use the tables `staff` and `address`:
+SELECT * FROM staff;
+SELECT * FROM address;
+SELECT staff.first_name, staff.last_name, address.address FROM staff JOIN address ON address.address_id=staff.address_id;
+
 
 -- 6b. Use `JOIN` to display the total amount rung up by each staff member in August of 2005. Use tables `staff` and `payment`.
-
+SELECT staff.first_name, staff.last_name, SUM(payment.amount)
+FROM staff 
+INNER JOIN payment on staff.staff_id=payment.staff_id
+AND payment_date LIKE '2005-08%'
+GROUP BY staff.staff_id;
 -- 6c. List each film and the number of actors who are listed for that film. Use tables `film_actor` and `film`. Use inner join.
 
 -- 6d. How many copies of the film `Hunchback Impossible` exist in the inventory system?
